dedc439cb69e9f19f08edc2f8c0ed87a7460c4ae
azema008 <azemjaparova@gmail.com>
2024-09-21 12:10:28 +0600

    first commit

---
+++ lesson_6.py
@@ -0,0 +1,56 @@
+'''objects = (True, 6.13, "python", 12)
+print(objects)
+print(type(objects))'''
+
+'''objects = (True, 6.13, "python", 12)
+objects = list(objects)
+print(objects)'''
+'''objects = tuple(item for item in objects if item != "python")
+print(objects)'''
+
+
+'''objects = objects + ("Java" ,)
+print(objects)'''
+'''key-ключ
+value-значения'''
+'izmena'
+'''student["age"] = 50
+student["hobby"][1] = "wrestling"'''
+
+'udalit'
+
+'''del student["name""]
+deleted = student.pop("age")
+del student["hobby"][1]'''
+
+'''print(student.clear())'''
+
+
+
+'''students = {
+ 'Айбек': [5, 4, 3],
+ 'Бурул': [4, 5, 5],
+ 'Гульназ': [3, 4, 4],
+ 'Дастан': [5, 5, 4],
+ 'Элиза': [4, 3, 5]
+}
+names = input('Введите имена студентов: ').split()
+scores = int(input('Введите средний балл студентов: ').split())
+object = (names + scores)
+print(object)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson_7.py
@@ -0,0 +1,10 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+elements = list(set(a) & set(b))
+print(elements)
+'''def send_hello(name, age):
+send_hello("Azemgul", 6'''
+
+
+
